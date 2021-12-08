ce19e585dbeb906e9c63dd8fb08eed4ee3c80533
Vincent Parizet <vincent.parizet@hotmail.fr>
2021-12-08 20:00:19 +0100

    oups

--- 2021/day6/lanternfish.zig
+++ 2021/day6/lanternfish.zig
@@ -3,53 +3,48 @@ const expect = std.testing.expect;
 const allocator = std.heap.page_allocator;
 
 const Result = struct {
-    score: i32 = 0,
+    score: u64 = 0,
 };
 
-const FLOOR_SIZE: usize = 1024;
-
 pub fn process(input: []const u8) !Result {
-    var floor: [FLOOR_SIZE][FLOOR_SIZE]i32 = undefined;
-    for (floor) |row, i_row| {
-        for (row) |_, i_col| {
-            floor[i_row][i_col] = 0;
-        }
-    }
+    var fish_count_per_age = [_]u64{0} ** 9;
 
-    var lines = std.mem.split(u8, input, "\n");
     const stdout = std.io.getStdOut().writer();
+    var fish_iter = std.mem.tokenize(u8, input, ",\n ");
 
     while (true) {
-        const line = lines.next() orelse break;
-        var positions_iter = std.mem.tokenize(u8, line, " ->,");
-
-        var x1 = try std.fmt.parseInt(i32, positions_iter.next().?, 10);
-        var y1 = try std.fmt.parseInt(i32, positions_iter.next().?, 10);
-        const x2 = try std.fmt.parseInt(i32, positions_iter.next().?, 10);
-        const y2 = try std.fmt.parseInt(i32, positions_iter.next().?, 10);
-
-        std.debug.assert(x1 < FLOOR_SIZE and y1 < FLOOR_SIZE and x2 < FLOOR_SIZE and y2 < FLOOR_SIZE);
-
-        var x_step: i32 = if (x2 > x1) 1 else -1;
-        var y_step: i32 = if (y2 > y1) 1 else -1;
-        if (x1 == x2) x_step = 0;
-        if (y1 == y2) y_step = 0;
-        while (true) {
-            floor[@intCast(u32, y1)][@intCast(u32, x1)] += 1;
-            if (x1 == x2 and y1 == y2) break;
-            x1 += x_step;
-            y1 += y_step;
-        }
+        const fish_str = fish_iter.next() orelse break;
+        const fish_age = try std.fmt.parseInt(u8, fish_str, 10);
+        fish_count_per_age[fish_age] += 1;
     }
 
-
-    var result = Result{};
-    for (floor) |row| {
-        for (row) |value| {
-            result.score += @boolToInt(value >= 2);
-
+    var i_day: u32 = 0;
+    while (i_day < 256) : (i_day += 1) {
+        if (false) {
+        try stdout.print("day {} - 0:{} 1:{} 2:{} 3:{} 4:{} 5:{} 6:{} 7:{} 8:{}\n", .{
+            i_day,
+            fish_count_per_age[0], fish_count_per_age[1], fish_count_per_age[2], fish_count_per_age[3],
+            fish_count_per_age[4], fish_count_per_age[5], fish_count_per_age[6], fish_count_per_age[7],
+            fish_count_per_age[8],
+        });
         }
 
+        const new_fishes = fish_count_per_age[0];
+        fish_count_per_age[0] = fish_count_per_age[1];
+        fish_count_per_age[1] = fish_count_per_age[2];
+        fish_count_per_age[2] = fish_count_per_age[3];
+        fish_count_per_age[3] = fish_count_per_age[4];
+        fish_count_per_age[4] = fish_count_per_age[5];
+        fish_count_per_age[5] = fish_count_per_age[6];
+        fish_count_per_age[6] = fish_count_per_age[7] + new_fishes;
+        fish_count_per_age[7] = fish_count_per_age[8];
+        fish_count_per_age[8] = new_fishes;
+    }
+
+    var result = Result{};
+    var i_count: usize = 0;
+    while (i_count < 9) : (i_count += 1) {
+        result.score += fish_count_per_age[i_count];
     }
     try stdout.print("\nscore: {}\n", .{result.score});
     return result;
@@ -68,18 +63,7 @@ pub fn main() !void {
 }
 
 test "example" {
-    const result = try process(
-        \\0,9 -> 5,9
-        \\8,0 -> 0,8
-        \\9,4 -> 3,4
-        \\2,2 -> 2,1
-        \\7,0 -> 7,4
-        \\6,4 -> 2,0
-        \\0,9 -> 2,9
-        \\3,4 -> 1,4
-        \\0,0 -> 8,8
-        \\5,5 -> 8,2
-    );
+    const result = try process("3,4,3,1,2");
 
-    try expect(result.score == 12);
+    try expect(result.score == 26984457539);
 }
